f052981b29f87143a4535c8ba70ec9be9df1cf07
Rehan Rashad <rehan.rashad@curemd.com>
2024-08-02 12:03:29 +0500

    UI Changes

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,11 +5,13 @@ import { HeaderComponent } from "../components/header/header.component";
 import { SignUp } from "../components/signup/signup.component";
 import { UserComponent } from "../components/user/user.component";
 import { SharedModule } from "../sharedmodules/shared.module";
+import { NewUserComponent } from "../components/new-user/new-user.component";
 import { TaskModule } from "./task.module";
 import { HttpClientModule } from "@angular/common/http";
 @NgModule({
-    declarations : [AppComponent,HeaderComponent,UserComponent,SignUp], // for non-standalone components
-    imports : [BrowserModule,TaskModule,SharedModule,HttpClientModule], //  for standalone components
+    
+    declarations : [AppComponent,HeaderComponent,UserComponent,SignUp,NewUserComponent], // for non-standalone components
+    imports : [BrowserModule,TaskModule,SharedModule], //  for standalone components
     bootstrap : [AppComponent]
 })
 export class AppModule{
